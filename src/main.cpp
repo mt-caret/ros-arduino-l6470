83df570a5b1cd6fba0b44e0d6f738982090ef05f
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2018-12-17 16:15:05 +0900

    add basic functionality

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #define USE_USBCON
 
 #include <ros.h>
-#include <std_msgs/String.h>
+#include <robot_lab/MotorCommand.h>
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13
@@ -14,30 +14,45 @@ const int SPI_CS_LEFT = 8, SPI_CS_RIGHT = 9;
 L6470 leftMotor(SPI_CS_LEFT), rightMotor(SPI_CS_RIGHT);
 
 ros::NodeHandle nh;
-std_msgs::String str_msg;
-ros::Publisher chatter("chatter", &str_msg);
 
-char hello[] = "hello, world!";
+Direction numberToDirection(float number) {
+  return number >= 0.0 ? Direction::forward : Direction::reverse;
+}
+
+void motorCommandCallback(const robot_lab::MotorCommand& msg) {
+  if (msg.left == 0.0 && msg.right == 0.0) {
+    if (msg.hardStop) {
+      leftMotor.hardStop();
+      rightMotor.hardStop();
+    } else {
+      leftMotor.softStop();
+      rightMotor.softStop();
+    }
+    return;
+  }
+  leftMotor.run(numberToDirection(msg.left), abs(msg.left));
+  rightMotor.run(numberToDirection(msg.right), abs(msg.right));
+}
+
+ros::Subscriber<robot_lab::MotorCommand> subscriber("motor", &motorCommandCallback);
 
 void setup() {
   nh.initNode();
-  nh.advertise(chatter);
+  nh.subscribe(subscriber);
 
-  //Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
-  //leftMotor.initialize();
-  //rightMotor.initialize();
-  //leftMotor.setStepMode(StepMode::microstep_128);
-  //rightMotor.setStepMode(StepMode::microstep_128);
-  //leftMotor.setKVal(KVal::hold, 255);
-  //leftMotor.setKVal(KVal::run, 255);
-  //leftMotor.setKVal(KVal::acc, 255);
-  //leftMotor.setKVal(KVal::dec, 255);
+
+  leftMotor.initialize();
+  rightMotor.initialize();
+  leftMotor.setStepMode(StepMode::microstep_128);
+  rightMotor.setStepMode(StepMode::microstep_128);
+  leftMotor.setKVal(KVal::hold, 255);
+  leftMotor.setKVal(KVal::run, 255);
+  leftMotor.setKVal(KVal::acc, 255);
+  leftMotor.setKVal(KVal::dec, 255);
 }
 
 void loop() {
-  str_msg.data = hello;
-  chatter.publish(&str_msg);
   nh.spinOnce();
   //digitalWrite(LED_BUILTIN, HIGH);
   //leftMotor.run(Direction::forward, 4000);
@@ -51,6 +66,5 @@ void loop() {
   //digitalWrite(LED_BUILTIN, LOW);
   //leftMotor.softStop();
   //rightMotor.softStop();
-
-  delay(1000);
+  delay(1);
 }
