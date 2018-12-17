6bcb6c65acf7e844306aef712d0807f509031cf6
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2018-12-17 14:29:09 +0900

    try out rosserial_arduino demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "Arduino.h"
 #include <l6470.h>
 
+#define USE_USBCON
+
+#include <ros.h>
+#include <std_msgs/String.h>
+
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13
 #endif
@@ -8,44 +13,44 @@
 const int SPI_CS_LEFT = 8, SPI_CS_RIGHT = 9;
 L6470 leftMotor(SPI_CS_LEFT), rightMotor(SPI_CS_RIGHT);
 
+ros::NodeHandle nh;
+std_msgs::String str_msg;
+ros::Publisher chatter("chatter", &str_msg);
+
+char hello[] = "hello, world!";
+
 void setup() {
-  Serial.begin(9600);
+  nh.initNode();
+  nh.advertise(chatter);
+
+  //Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
-  leftMotor.initialize();
-  rightMotor.initialize();
-  leftMotor.setStepMode(StepMode::microstep_128);
-  rightMotor.setStepMode(StepMode::microstep_128);
-  leftMotor.setKVal(KVal::hold, 255);
-  leftMotor.setKVal(KVal::run, 255);
-  leftMotor.setKVal(KVal::acc, 255);
-  leftMotor.setKVal(KVal::dec, 255);
+  //leftMotor.initialize();
+  //rightMotor.initialize();
+  //leftMotor.setStepMode(StepMode::microstep_128);
+  //rightMotor.setStepMode(StepMode::microstep_128);
+  //leftMotor.setKVal(KVal::hold, 255);
+  //leftMotor.setKVal(KVal::run, 255);
+  //leftMotor.setKVal(KVal::acc, 255);
+  //leftMotor.setKVal(KVal::dec, 255);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("Sending Run command");
-  leftMotor.run(Direction::forward, 4000);
-  rightMotor.run(Direction::forward, 4000);
+  str_msg.data = hello;
+  chatter.publish(&str_msg);
+  nh.spinOnce();
+  //digitalWrite(LED_BUILTIN, HIGH);
+  //leftMotor.run(Direction::forward, 4000);
+  //rightMotor.run(Direction::forward, 4000);
 
-  //leftMotor.updateStatus();
-  //leftMotor.printStatus();
+  ////leftMotor.updateStatus();
+  ////leftMotor.printStatus();
 
-  delay(10000);
+  //delay(10000);
 
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("Sending SoftStop command");
-  leftMotor.softStop();
-  rightMotor.softStop();
-
-  Serial.print("getParam result: ");
-  Serial.print(leftMotor.getParam(REG_CONFIG), HEX);
-  Serial.print('\n');
-
-  //leftMotor.updateStatus();
-  //leftMotor.printStatus();
+  //digitalWrite(LED_BUILTIN, LOW);
+  //leftMotor.softStop();
+  //rightMotor.softStop();
 
   delay(1000);
-  Serial.print("getStatus result: ");
-  Serial.print(leftMotor.getStatus(), HEX);
-  Serial.print('\n');
 }
